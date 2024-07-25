0365340441a7d5636ce5c72d4944d99f2c9170f6
erdemyesilcicek <erdmyslcck9@outlook.com>
2024-07-25 16:51:53 +0300

    modified the all system.

--- app/src/main/java/com/erdemyesilcicek/sentinel/GenerateFragment.kt
+++ app/src/main/java/com/erdemyesilcicek/sentinel/GenerateFragment.kt
@@ -4,15 +4,16 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
+import android.text.Layout.Directions
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SeekBar
 import android.widget.Toast
+import androidx.navigation.Navigation
 import com.erdemyesilcicek.sentinel.databinding.FragmentGenerateBinding
 import kotlin.random.Random
-
 class GenerateFragment : Fragment() {
     private var _binding: FragmentGenerateBinding? = null
     private val binding get() = _binding!!
@@ -33,22 +34,10 @@ class GenerateFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                binding.seekbarText.text = progress.toString()
-            }
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                if(seekBar != null){
-                    startPoint = seekBar.progress
-                }
-            }
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                if(seekBar != null){
-                    endPoint = seekBar.progress
-                }
-            }
-        })
-
+        binding.generateButton.setOnClickListener { generateButtonClicked(it) }
+        seekBarController()
+    }
+    fun generateButtonClicked(view: View){
         binding.apply {
             generateButton.setOnClickListener {
                 val selectedOptions = mutableListOf<Char>()
@@ -69,7 +58,13 @@ class GenerateFragment : Fragment() {
                     Toast.makeText(requireContext(), "please select options.", Toast.LENGTH_LONG).show()
                     return@setOnClickListener
                 }
+
                 val passwordLength = binding.seekbarText.text.toString().toInt()
+
+                if(checkboxLowercase.isChecked && checkboxUppercase.isChecked && checkboxNumbers.isChecked && checkboxSymbols.isChecked && passwordLength >= 10){
+                    Toast.makeText(requireContext(), "YOUR PASSWORD IS SO STRONG!", Toast.LENGTH_SHORT).show()
+                }
+
                 val randomPassword = buildString {
                     repeat(passwordLength){
                         val randomIndex = Random.nextInt(0,selectedOptions.size)
@@ -85,6 +80,23 @@ class GenerateFragment : Fragment() {
                 Toast.makeText(requireContext(),"copied.", Toast.LENGTH_LONG).show()
             }
         }
+    }
+    private fun seekBarController(){
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.seekbarText.text = progress.toString()
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                if(seekBar != null){
+                    startPoint = seekBar.progress
+                }
+            }
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                if(seekBar != null){
+                    endPoint = seekBar.progress
+                }
+            }
+        })
     }
     override fun onDestroyView() {
         super.onDestroyView()
